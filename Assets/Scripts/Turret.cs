c46241de5cbe970b16c15438648b4ca5757b3b92
Reece-E <144268748+Reece-E@users.noreply.github.com>
2023-10-10 11:42:22 -0700

    did turret stuf

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Turret : MonoBehaviour
+{
+    public GameObject player;
+
+    public GameObject turretprojectile;
+
+    public float initialtime;
+
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialtime = timer;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"));
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < 0);
+        {
+            timer = initialtime;
+            GameObject projectile = Instantiate(turretprojectile, transform.position, Quaternion.identity);
+             Vector3 target = player.transform.position;
+             projectile.transform.Translate(target);
+            
+            
+        }
+    }
+}
