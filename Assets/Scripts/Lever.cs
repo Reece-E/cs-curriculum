497b13dd6498afdef96dd8af44ad6c24c5f7139c
Reece-E <144268748+Reece-E@users.noreply.github.com>
2024-02-15 12:01:26 -0800

    nothing

---
+++ Assets/Scripts/Lever.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    public bool leverStatus;
+
+    private Animator m_Animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (gameObject.CompareTag("Player"))
+        {
+        
+            leverStatus = true;
+
+            m_Animator.SetBool("Active", true);
+        }
+    }
+
+    
+        
+    
+
+}
+
